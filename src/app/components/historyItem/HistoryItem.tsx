91349cb6eea07e1b73948e2faa1609aec681d6ea
TIMOTHY WOOD <timwood46@gmail.com>
2025-02-27 22:29:46 +0000

    history item works

--- src/app/components/historyItem/HistoryItem.tsx
+++ src/app/components/historyItem/HistoryItem.tsx
@@ -8,19 +8,16 @@ type Props = {
 
 const HistoryItem = ({ onClick }: Props) => {
   return (
-    <div className="historyItem bg-surface-secondary items-space-between hover:bg-surface-tertiary w-100 flex rounded">
-      <button
-        className="bg-inheritHistoryItem min-w-0 flex-1 p-2 text-left"
-        onClick={onClick}
-      >
+    <div className="historyItem bg-surface-secondary hover:bg-surface-tertiary w-100 grid items-center overflow-hidden rounded-lg">
+      <button className="p-2 text-left" onClick={onClick}>
         <Paragraph
-          size="xs"
+          size="2xs"
           className="pointer-events-none relative overflow-hidden whitespace-nowrap before:absolute before:right-0 before:top-0 before:h-full before:w-[50px]"
         >
           {"Create React Typescript boilerplate for a Next.js app"}
         </Paragraph>
       </button>
-      <button className="rounded p-2">
+      <button className="historyItem__menuBtn mx-2 rounded-full transition-all duration-200">
         <EllipsisHorizontal />
       </button>
     </div>
